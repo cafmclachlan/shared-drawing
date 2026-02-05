4005022ad899a163df7886992b94e49286733f32
Charlotte McLachlan <cafmclachlan@gmail.com>
2026-02-05 16:13:37 +1100

    Fix Render port binding

--- server.js
+++ server.js
@@ -46,6 +46,9 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(3000, () => {
-  console.log("Server running at http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+const HOST = "0.0.0.0";
+
+server.listen(PORT, HOST, () => {
+  console.log(`Server running on ${HOST}:${PORT}`);
 });
